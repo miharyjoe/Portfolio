7c3acee9ee80759211183f0dcfc94ea8426c9830
miharyjoel <miharyjoel@gmail.com>
2023-05-10 23:00:56 +0300

    feat: add about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,22 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
 import React from "react";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <Head>
+        <title>Mihary Joel | About page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout>
+          <AnimatedText text="It lovers" />
+        </Layout>
+      </main>
+    </>
+  );
 };
 
 export default about;
